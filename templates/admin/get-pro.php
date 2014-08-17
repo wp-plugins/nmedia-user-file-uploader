32656fc901741101d738ddc8006b8fff60c18571
nmedia <nmedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-17 07:31:52 +0000

    reloading page once files are saved.
    git-svn-id: https://plugins.svn.wordpress.org/nmedia-user-file-uploader/tags/3.2@967241 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/admin/get-pro.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * this is just to explain PRO features
+ */
+
+$proFeatures .= '<p>';
+$proFeatures .= '<h2>'.__('Best Photo Editing Tool', 'nm-filemanager').'</h2>';
+$proFeatures .= __('This plugin can be used as best Photo Editing Tool to allow your users to upload and Edit Photos online using world\'s best Photo Editor \'Aviary\'. There is an Add-on for this','nm-filemanager');
+$proFeatures .= '<br><br><a class="button" target="_blank" href="http://www.aviary.com/">'.__('What is Aviary?','nm-filemanager').'</a>';
+$proFeatures .= ' <a class="button-primary" href="">'.__('See Demo','nm-filemanager').'</a>';
+$proFeatures .= ' <a class="button-primary" href="">'.__('Get Pro + Addon','nm-filemanager').'</a>';
+$proFeatures .= '</p>';
+
+
+$proFeatures .= '<br>More information contact: <a href="mailto:sales@najeebmedia.com">sales@najeebmedia.com</a>';
+
+//echo $proFeatures;
+?>
+
+<h2>Get PRO Version for only $30.00</h2>
+<ul>
+	<li>Min files limit</li>
+	<li>Max file upload limit set</li>
+	<li>Secure download link</li>
+	<li>Admin email notification/alert</li>
+	<li>Share file (user can send file in email)</li>
+	<li>File meta attachment (Awesome feature)</li>
+	<li>File sorting, searching, pagination</li>
+	<li>Admin: disable/enable upload/download section</li>
+	<li>Admin: Allow users to upload files even not registered (public)</li>
+	<li>Access to support forum</li>	
+</ul>
+
+<p>Pro version is under testing. If you want to receive alert on it's launch, please subscribe to following link and get <strong>50% discount via promo code</strong>.</p>
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="//najeebmedia.us2.list-manage.com/subscribe/post?u=8dea921597aa416ee5ec39cfc&amp;id=8d79acb2d0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+	<label for="mce-EMAIL">Subscribe for File upload and manager plugin</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_8dea921597aa416ee5ec39cfc_8d79acb2d0" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
