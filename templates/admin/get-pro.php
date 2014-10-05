17f687cfd9cf81c9136c43fffbdce254322a149a
nmedia <nmedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-05 13:06:52 +0000

    file uploader new version just launched.
    git-svn-id: https://plugins.svn.wordpress.org/nmedia-user-file-uploader/tags/3.5@1001995 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/admin/get-pro.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * this is just to explain PRO features
+ */
+
+$proFeatures .= '<p>';
+$proFeatures .= '<h2>'.__('Best Photo Editing Tool', 'nm-filemanager').'</h2>';
+$proFeatures .= __('This plugin can be used as best Photo Editing Tool to allow your users to upload and Edit Photos online using world\'s best Photo Editor \'Aviary\'. There is an Add-on for this','nm-filemanager');
+$proFeatures .= '<br><br><a class="button" target="_blank" href="http://www.aviary.com/">'.__('What is Aviary?','nm-filemanager').'</a>';
+$proFeatures .= ' <a class="button-primary" href="">'.__('See Demo','nm-filemanager').'</a>';
+$proFeatures .= ' <a class="button-primary" href="">'.__('Get Pro + Addon','nm-filemanager').'</a>';
+$proFeatures .= '</p>';
+
+
+$proFeatures .= '<br>More information contact: <a href="mailto:sales@najeebmedia.com">sales@najeebmedia.com</a>';
+
+//echo $proFeatures;
+?>
+
+<h2>Get PRO Version for only $35.00</h2>
+<ul>
+	<li>Min files limit</li>
+	<li>Max file upload limit set</li>
+	<li>Secure download link</li>
+	<li>Admin email notification/alert</li>
+	<li>Share file (user can send file in email)</li>
+	<li>File meta attachment (Awesome feature)</li>
+	<li>File sorting, searching, pagination</li>
+	<li>Admin: disable/enable upload/download section</li>
+	<li>Admin: Allow users to upload files even not registered (public)</li>
+	<li>Access to support forum</li>	
+</ul>
+
+<a href="http://www.najeebmedia.com/nmedia-file-uploader-pro/">Buy Now</a>
+
